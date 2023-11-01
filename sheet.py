944e210a1dc2778c5a1903b93b68972351af0894
Rohit Kumar <kumar1rohit@outlook.com>
2023-11-01 23:49:24 +0530

    add env from .env instead of json file

--- sheet.py
+++ sheet.py
@@ -6,10 +6,24 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+credentials_dict = {
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY"),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN"),
+}
+
 
 class GoogleSheet:
     def __init__(self, headers=[]):
-        self.gs = gspread.service_account(filename="credentials.json")
+        self.gs = gspread.service_account_from_dict(credentials_dict)
         self.spreadsheet_id = os.getenv("GOOGLE_SPREADSHEET_ID")
         self.headers = headers
         self.ws_format = {
